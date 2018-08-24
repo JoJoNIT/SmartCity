977726508118f8c2ceef12720fcdf4e6261753a7
JoJoNIT <1798164684@qq.com>
2018-08-24 17:56:12 +0800

    增加AsyncTask

---
+++ app/src/main/java/com/wzlab/smartcity/activity/main/DeviceDetailActivity.java
@@ -0,0 +1,85 @@
+package com.wzlab.smartcity.activity.main;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+
+import com.baidu.mapapi.SDKInitializer;
+import com.wzlab.smartcity.R;
+import com.wzlab.smartcity.widget.ColorfulProgressBar;
+
+public class DeviceDetailActivity extends AppCompatActivity {
+
+    private TextView mTvDeviceId;
+    private TextView mTvAddr;
+    private TextView mTvHeadName;
+    private TextView mTvHeadPhone;
+    private TextView mTvPoliceStation;
+    private Switch mSwitchDeviceStatus;
+    private Button mBtnhandle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+
+        setContentView(R.layout.activity_device_detail);
+        initView();
+        initData();
+    }
+
+    private void initData() {
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String user_id = bundle.getString("user_id","暂无数据");
+        String user_address = bundle.getString("user_address","暂无数据");
+        String head = bundle.getString("head","暂无数据");
+        String head_phone = bundle.getString("head_phone","暂无数据");
+        String police_station = bundle.getString("police_station","暂无数据");
+        mTvDeviceId.setText(user_id);
+        mTvAddr.setText("定位信息:" + user_address);
+        mTvHeadName.setText("姓名：" + head);
+        mTvHeadPhone.setText("联系电话：" + head_phone);
+        mTvPoliceStation.setText("工作单位：" + police_station);
+        String deviceStatus = bundle.getString("device_status","1");
+        if(deviceStatus.equals("1")){
+             mSwitchDeviceStatus.setChecked(true);
+        }else {
+            mSwitchDeviceStatus.setChecked(false);
+        }
+        mSwitchDeviceStatus.setEnabled(false);
+    }
+
+    private void initView() {
+        Toolbar toolbar = findViewById(R.id.tb_device_detail);
+        toolbar.setTitle("设备详情");
+        mTvDeviceId = findViewById(R.id.tv_device_num);
+        mTvAddr = findViewById(R.id.tv_device_location);
+        mTvHeadName = findViewById(R.id.tv_principal_name);
+        mTvHeadPhone = findViewById(R.id.tv_principal_phone);
+        mTvPoliceStation = findViewById(R.id.tv_principal_police);
+        mSwitchDeviceStatus = findViewById(R.id.switch_device_status);
+        mBtnhandle = findViewById(R.id.btn_handle);
+
+        mBtnhandle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DeviceDetailActivity.this,HandleAlarmActivity.class));
+                finish();
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/wzlab/smartcity/activity/main/HandleAlarmActivity.java
@@ -0,0 +1,170 @@
+package com.wzlab.smartcity.activity.main;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.skateboard.zxinglib.CaptureActivity;
+import com.wzlab.smartcity.R;
+import com.wzlab.smartcity.activity.account.Config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class HandleAlarmActivity extends AppCompatActivity {
+    private Spinner spinner;
+    private Button mBtnsubmit;
+    private ImageView mIvpicture;
+    private EditText mEtid,mEtdetail;
+    private String deviceInfo,cardNumber;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handle_alarm);
+        text=findViewById(R.id.tv_http);
+        findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SubmitURL(Config.SERVER_URL+Config.ACTION_UPLOAD_REPAIR_RESULT);
+            }
+        });
+
+        String[] ctype = new String[]{"请选择故障类型", "摄像头损坏", "APP出现BUG", "网络问题"};
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ctype);  //创建一个数组适配器
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);     //设置下拉列表框的下拉选项样式
+        spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String cardNumber = spinner.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        mEtid = findViewById(R.id.et_id);
+
+        mEtdetail=findViewById(R.id.et_detail);
+        mBtnsubmit = findViewById(R.id.btn_submit);
+        mBtnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String deviceId = mEtid.getText().toString().trim();
+                String detail = mEtdetail.getText().toString().trim();
+
+
+
+                Toast.makeText(HandleAlarmActivity.this,"提交成功"+ deviceId+cardNumber +detail,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mIvpicture = findViewById(R.id.iv_picture);
+        mIvpicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HandleAlarmActivity.this,CaptureActivity.class);
+                startActivityForResult(intent,1001);
+
+
+            }
+
+        });
+
+    }
+    public void SubmitURL(String url){
+        new AsyncTask<String,Float,String>(){
+
+            @Override
+            protected String doInBackground(String... strings) {
+                String phone="18392888103";
+                String device_id="";
+                String result_description="";
+                try {
+                    URL url = new URL(strings[0]);
+                    URLConnection connection=url.openConnection();
+                    long total = connection.getContentLength();
+                    InputStream inputStream = connection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(inputStream);
+                    BufferedReader br = new BufferedReader(isr);
+                    String line;
+                    StringBuilder builder = new StringBuilder();
+                    while((line=br.readLine())!=null){
+                        builder.append(line);
+                        publishProgress((float)builder.toString().length()/total);
+                    }
+                    br.close();
+                    inputStream.close();
+                    return builder.toString();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onCancelled() {
+                super.onCancelled();
+            }
+
+            @Override
+            protected void onCancelled(String s) {
+                super.onCancelled(s);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                text.setText(s);
+                super.onPostExecute(s);
+            }
+
+            @Override
+            protected void onPreExecute() {
+                Toast.makeText(HandleAlarmActivity.this,"开始读取",Toast.LENGTH_SHORT).show();
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onProgressUpdate(Float... values) {
+                System.err.println(values[0]);
+                super.onProgressUpdate(values);
+            }
+        }.execute(url);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==1001 && resultCode== Activity.RESULT_OK)
+        {
+            deviceInfo = data.getStringExtra(CaptureActivity.KEY_DATA);
+            mEtid.setText(deviceInfo.split("#")[0]);
+            Toast.makeText(HandleAlarmActivity.this, deviceInfo, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
+
